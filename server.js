c650de61794ee7224eff7a14f7a739bed22aaef9
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-07-19 18:35:19 +0800

    testing for res json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("aws testing");
+  res.json("aws testing");
 });
 
 const PORT = process.env.PORT || 3000;
